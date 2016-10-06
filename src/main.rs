77033683c6c175dbd8fa45377c9138fc02989a3d
Matt Pindell <pindell.m@gmail.com>
2016-10-05 23:51:05 -0400

    Solution (#4)
    * Add generate_solution and calc_manhattan_distance functions
    
    * Add cell_data and cells_match functions
    
    * Add work in progress for most_productive_path
    
    * Add reset_maze fn
    
    * Basic rough implementation of solution working

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,36 @@ extern crate rand;
 
 use rand::{thread_rng, Rng};
 
-static SIZE: usize = 8;
+static SIZE: usize = 4;
 
 fn main() {
-    let maze = generate_maze();
+    let mut maze = generate_maze();
     print_to_console(&maze);
+
+    // set visited status to 0
+    let mut solution = reset_maze(maze);
+
+    let result = generate_solution(&solution);
+
+    // temporary data check
+    for row in result {
+        println!("{:?}", row);
+    }
+
+}
+
+fn reset_maze(mut maze: Vec<Vec<Vec<i32>>>) -> Vec<Vec<Vec<i32>>> {
+    let mut solution = vec![];
+    for row in maze {
+        let mut new_row = vec![];
+        for cell in row {
+            let mut clone_cell = cell.clone();
+            clone_cell[4] = 0;
+            new_row.push(clone_cell);
+        }
+        solution.push(new_row);
+    }
+    solution
 }
 
 fn generate_maze() -> Vec<Vec<Vec<i32>>> {
@@ -89,6 +114,94 @@ fn shuffle_contents(check: Vec<String>) -> Vec<String> {
 
 fn are_equal(move_direction: &String, direction: char) -> bool {
     move_direction.to_string() == direction.to_string()
+}
+
+fn generate_solution(grid: &Vec<Vec<Vec<i32>>>) -> Vec<Vec<Vec<i32>>>{
+    let mut solution = grid.clone();
+    let src = (0, 0);
+    let dst = ((SIZE - 1) as i32, (SIZE - 1) as i32);
+
+    let mut best_md = manhattan_dist(src, dst);
+    let mut current = (0, 0);
+    solution[(current.0) as usize][(current.1) as usize][4] = 1;
+
+    while !cells_match(current, dst) {
+        if productive_path_available(&solution, current, best_md, dst) {
+            let path = most_productive_path(&solution, current, cell_data(&solution, current), best_md, dst);
+            current = path[0].1;
+            best_md = manhattan_dist(current, dst);
+            solution[(current.0) as usize][(current.1) as usize][4] = 1;
+        } else {
+            current = options(&solution, current);
+            solution[(current.0) as usize][(current.1) as usize][4] = 1;
+
+            println!("current: {:?}", current);
+            println!("option: {:?}", options(&solution, current));
+            println!("productive option: {:?}", most_productive_path(&solution, current, cell_data(&solution, current), best_md, dst));
+        }
+    }
+    solution
+}
+
+fn options(grid: &Vec<Vec<Vec<i32>>>, curr: (i32, i32)) -> (i32, i32) {
+    let data = cell_data(&grid, curr);
+    let mut results = curr;
+    let left  = (curr.0, curr.1 - 1);
+    let right = (curr.0, curr.1 + 1);
+    let up    = (curr.0 - 1, curr.1);
+    let down  = (curr.0 + 1, curr.1);
+
+    if data[0] == 1 && grid[(left.0) as usize][(left.1) as usize][4] == 0 { results = left };
+    if data[1] == 1 && grid[(up.0) as usize][(up.1) as usize][4] == 0 { results = up };
+    if data[2] == 1 && grid[(right.0) as usize][(right.1) as usize][4] == 0 { results = right };
+    if data[3] == 1 && grid[(down.0) as usize][(down.1) as usize][4] == 0 { results = down };
+
+    results
+}
+
+fn most_productive_path(grid: &Vec<Vec<Vec<i32>>>, curr: (i32, i32), curr_data: Vec<i32>, best_md: i32, dst: (i32, i32)) -> Vec<(i32, (i32, i32))> {
+    let mut paths = vec![];
+    let left  = (curr.0, curr.1 - 1);
+    let right = (curr.0, curr.1 + 1);
+    let up    = (curr.0 - 1, curr.1);
+    let down  = (curr.0 + 1, curr.1);
+
+    if curr_data[0] == 1 && manhattan_dist(left, dst) <= best_md && grid[(left.0) as usize][(left.1) as usize][4] == 0 { paths.push( (manhattan_dist(left, dst), left) ); }
+    if curr_data[1] == 1 && manhattan_dist(up, dst) <= best_md && grid[(up.0) as usize][(up.1) as usize][4] == 0 { paths.push( (manhattan_dist(up, dst), up) ); }
+    if curr_data[2] == 1 && manhattan_dist(right, dst) <= best_md && grid[(right.0) as usize][(right.1) as usize][4] == 0 { paths.push( (manhattan_dist(right, dst), right) ); }
+    if curr_data[3] == 1 && manhattan_dist(down, dst) <= best_md && grid[(down.0) as usize][(down.1) as usize][4] == 0 { paths.push( (manhattan_dist(down, dst), down) ); }
+
+    paths.sort();
+    paths
+}
+
+fn productive_path_available(grid: &Vec<Vec<Vec<i32>>>, curr: (i32, i32), best_md: i32, dst: (i32, i32)) -> bool {
+    let curr_data = cell_data(grid, curr);
+    let left  = (curr.0, curr.1 - 1);
+    let right = (curr.0, curr.1 + 1);
+    let up    = (curr.0 - 1, curr.1);
+    let down  = (curr.0 + 1, curr.1);
+
+    if curr_data[0] == 1 && manhattan_dist(left, dst) <= best_md && grid[(left.0) as usize][(left.1) as usize][4] == 0 { return true; }
+    if curr_data[1] == 1 && manhattan_dist(up, dst) <= best_md && grid[(up.0) as usize][(up.1) as usize][4] == 0 { return true; }
+    if curr_data[2] == 1 && manhattan_dist(right, dst) <= best_md && grid[(right.0) as usize][(right.1) as usize][4] == 0 { return true; }
+    if curr_data[3] == 1 && manhattan_dist(down, dst) <= best_md && grid[(down.0) as usize][(down.1) as usize][4] == 0 { return true; }
+    false
+}
+
+fn cell_data(grid: &Vec<Vec<Vec<i32>>>, coordinates: (i32, i32)) -> Vec<i32> {
+    let ref data = grid[(coordinates.0) as usize][(coordinates.1) as usize];
+    data.clone()
+}
+
+fn cells_match(current: (i32, i32), destination: (i32, i32)) -> bool {
+    current.0 == destination.0 && current.1 == destination.1
+}
+
+fn manhattan_dist(a: (i32, i32), b: (i32, i32)) -> i32 {
+    let y_val = (a.0 - b.0).abs();
+    let x_val = (a.1 - b.1).abs();
+    x_val + y_val
 }
 
 fn print_to_console(grid: &Vec<Vec<Vec<i32>>>) {

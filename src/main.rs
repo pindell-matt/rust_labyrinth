d711e76c893e9d726ce3d92fc20cb1ecce0e039c
Matt Pindell <pindell.m@gmail.com>
2016-10-06 13:32:07 -0400

    Remove comments from main

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ use reset::reset_maze;
 use print::print_to_console;
 use solve::generate_solution;
 
-static SIZE: usize = 4;
-
 fn main() {
-    let maze = generate_maze(SIZE);
+    let maze = generate_maze(4);
 
     let unwalked_maze = reset_maze(maze);
     print_to_console(&unwalked_maze);
@@ -19,22 +17,3 @@ fn main() {
     let result = generate_solution(&unwalked_maze);
     print_to_console(&result);
 }
-
-// fn reset_maze(maze: Vec<Vec<Vec<i32>>>) -> Vec<Vec<Vec<i32>>> {
-//     let mut solution = vec![];
-//     for row in maze {
-//         let mut new_row = vec![];
-//         for cell in row {
-//             let mut clone_cell = cell.clone();
-//             clone_cell[4] = 0;
-//             new_row.push(clone_cell);
-//         }
-//         solution.push(new_row);
-//     }
-//
-//     // set start and exit
-//     solution[0][0][4] = 2;
-//     solution[SIZE - 1][SIZE - 1][3] = 1;
-//
-//     solution
-// }
